d92df46872e41ef223e665adff7af57dae6489d0
ljy2855 <ljy2855@naver.com>
2022-01-11 18:56:26 +0900

    init

---
+++ api/main.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+import os
+import subprocess
+from fastapi.middleware.cors import CORSMiddleware
+
+DIRPATH = os.getcwd()
+
+app = FastAPI()
+origins = [
+    "test.cspc.me"
+    "localhost:2000",
+    "localhost:3000",
+          ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class CodeFile(BaseModel):
+    code: str
+    
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/code")
+async def get_code(code : CodeFile):
+    codefile = dict(code)
+    codefile['success'] = True
+    
+    
+    return await compile_code(codefile)
+
+
+async def compile_code(codefile):
+    file_path = DIRPATH + "/template_code/test.c" 
+    f = open(file_path,"w")
+    f.write(codefile['code'])
+    f.close()
+    
+    try:
+        output =subprocess.check_output("gcc -o {0}/test {1}".format(DIRPATH+"/template_code",file_path),shell =True,stderr=subprocess.STDOUT,universal_newlines=True)
+        result = subprocess.check_output(DIRPATH +"/template_code/test", shell=True,timeout = 1,universal_newlines=True)
+    except subprocess.CalledProcessError as e: #컴파일 실패
+        result = ''
+        for line in e.output.split('\n'):
+            for word in line.split(' '):
+                if DIRPATH not in word:
+                    result += word + ' '
+            result += '\n'
+    except subprocess.TimeoutExpired as e:
+        if len(e.output) > 10000:
+            result = 'Warning: over 10000 char\n'
+            result += e.output.decode('utf-8')[:10000]
+        else:
+            result = e.output
+        
+    #print(result)
+    return result
+    
--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import AceEditor from "react-ace";
 
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-terminal";
+import React from 'react';
+import axios from 'axios';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Editor/>
   );
 }
+
+
+class Editor extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            code: '',
+            output: '',
+            
+        }
+        this.handleCodeChange = this.handleCodeChange.bind(this);
+        this.handleCompile = this.handleCompile.bind(this);
+
+    }
+    
+    handleCodeChange(code){
+       console.log(this.state.code);
+       this.setState({
+           code : code,
+       });
+        
+    }
+    
+    handleCompile(e){
+        e.preventDefault();
+
+
+        const data = this.state;
+        
+        axios.post(
+        'https://test.cspc.me/api/code',
+        {code: data.code},
+        {headers: { "Content-Type": `application/json`,
+                  }},
+            
+            
+        ).then((res)=> {
+               console.log(res)
+               this.setState({output : res.data});
+              }).catch((error) => {
+        console.log(error);
+        });
+        
+        /*
+        fetch("http://test.cspc.me/api/code",{
+            method: "POST",
+            mode: 'cors',
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json",
+                //"Access-Control-Allow-Headers" : "localhost:300",
+            },
+            body: {"code" : data.code},
+        }).then((res) => console.log(res));
+        */
+    }
+    render(){
+        return(
+        <div>
+            <div>
+            <span>
+                Code
+            </span>
+            <AceEditor
+                mode="c_cpp"
+                theme="monokai"
+                name="UNIQUE_ID_OF_DIV"
+                value= {this.state.code}
+                onChange={this.handleCodeChange}
+                editorProps={{ $blockScrolling: true }}
+                enableBasicAutocompletion={true}
+                enableLiveAutocompletion={true}
+                enableSnippets={true}
+            />
+            </div>
+            <div>
+            <button 
+                onClick={this.handleCompile}>
+                Submit
+            </button>
+            </div>
+            <span>
+                Output
+            </span>
+            <div>
+                <AceEditor
+                    theme="terminal"
+                    value= {this.state.output}
+                    editorProps={{ $blockScrolling: false }}
+                    readOnly = {true}
+                    height = "300px"
+            />
+            </div>
+        </div>
+        );
+    }
+}
+
+
 
 export default App;
